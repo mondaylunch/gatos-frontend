9497fd250cc9600b437d0b38a9435906bb8254c8
Paul Makles <me@insrt.uk>
2023-01-23 14:32:04 +0000

    feat: configure Tailwind CSS

--- packages/website/src/routes/index.tsx
+++ packages/website/src/routes/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "~/components/inputs/Button";
 import "./index.css";
 
 export default function Home() {
   return (
     <main>
       <h1>Gatos</h1>
+      <Button>text in the button</Button>
     </main>
   );
 }
