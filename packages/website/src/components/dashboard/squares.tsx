4830b85d777b3f1e14c160bfd789c605cb9e6742
Cosmo <k21090628@kcl.ac.uk>
2023-01-26 19:05:17 +0000

    feat: add file dash component

--- packages/website/src/components/dashboard/squares.tsx
+++ packages/website/src/components/dashboard/squares.tsx
@@ -1,10 +1,24 @@
-import type { JSX } from "solid-js";
+import type { JSX} from "solid-js";
 import "../icons.css";
+
+type DisplayProps = {
+    title: string;
+    description: string
+}
 
 export function Square_New() {
     return (
-<div class="relative h-32 w-32 rounded-[35px] border-dashed border-8 border-neutral-700 bg-transparent flex items-center justify-center hover:scale-110 hover:bg-neutral-300 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-00 transition duration-150 ease-in-out ">
-        <i class="bi bi-plus text-neutral-500 text-8xl"></i>
-</div>
+        <div class="relative h-32 w-32 rounded-[35px] border-dashed border-8 border-neutral-700 bg-transparent flex items-center justify-center hover:scale-110 hover:bg-neutral-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-00 transition duration-150 ease-in-out ">
+                <i class="bi bi-plus text-neutral-500 text-8xl"></i>
+        </div>
     )
     }
+
+export function Square_File(props: DisplayProps) {
+    return (
+        <div class="relative h-32 w-32 rounded-[35px] bg-neutral-900 flex items-center justify-center hover:scale-110 hover:bg-neutral-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-00 transition duration-150 ease-in-out ">
+                <p>{props.title}</p>
+                <p>{props.description}</p>
+        </div>
+    )
+}
--- packages/website/src/routes/dash.tsx
+++ packages/website/src/routes/dash.tsx
@@ -1,12 +1,18 @@
   import {
     Square_New,
+    Square_File
   } from "~/components/dashboard/squares";
   import "../components/icons.css";
   
   export default function Home() {
     return (
-        <div class="flex flex-col items-center justify-center w-screen h-screen">
+        <div class="flex flex-col items-center justify-center w-screen h-screen bg-neutral-800">
+          <div class="flex">
+            <div class="mr-5">
             <Square_New />
+            </div>
+            <Square_File title="Title of me" description="Look at my description" />
+          </div>
         </div>
     
     );
