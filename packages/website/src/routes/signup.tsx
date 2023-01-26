4fc5f3f71d3df469697afd2c8d0ef89ba7fecc49
Cosmo <k21090628@kcl.ac.uk>
2023-01-26 16:08:25 +0000

    fix: remove duplicate colour

--- packages/website/src/routes/signup.tsx
+++ packages/website/src/routes/signup.tsx
@@ -9,7 +9,7 @@ import {
     return (
     
         <div class="flex h-screen w-screen items-center justify-center bg-slate-700">
-            <div class="block pt-6 pr-6 pl-6 pb-4 rounded-lg shadow-lg bg-white max-w-sm bg-slate-800">
+            <div class="block pt-6 pr-6 pl-6 pb-4 rounded-lg shadow-lgmax-w-sm bg-slate-800">
             
                 <div class="pb-2">
                 <Username_Textbox id="username" />
