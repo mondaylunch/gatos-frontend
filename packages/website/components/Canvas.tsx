dcb39dc4f050d096d379cbee1d7fcca0c16c2c63
Paul Makles <me@insrt.uk>
2023-01-26 11:58:46 +0000

    feat(editor): mobile touch support

--- packages/website/components/Canvas.tsx
+++ packages/website/components/Canvas.tsx
@@ -16,33 +16,25 @@ export function Canvas() {
 
   // Handle panning of canvas
   const [pan, setPan] = createSignal(false);
-  const startPan = (event: MouseEvent | TouchEvent) => {
+
+  // Create event handlers for mouse panning
+  const onMouseDown = (event: MouseEvent) => {
     if (event.target instanceof SVGSVGElement) {
       event.preventDefault();
       setPan(true);
     }
   };
-  const stopPan = () => setPan(false);
-  const onPan = (event: MouseEvent | TouchEvent) => {
-    if (event instanceof MouseEvent) {
-      setMouseX(event.offsetX);
-      setMouseY(event.offsetY);
-    }
+
+  const onMouseUp = () => setPan(false);
+
+  const onMouseMove = (event: MouseEvent) => {
+    setMouseX(event.offsetX);
+    setMouseY(event.offsetY);
 
     if (pan()) {
       event.preventDefault();
-
-      setOriginX(
-        (ox) =>
-          ox -
-          (event instanceof MouseEvent ? event.movementX : 0) / zoomFactor()
-      );
-
-      setOriginY(
-        (oy) =>
-          oy -
-          (event instanceof MouseEvent ? event.movementY : 0) / zoomFactor()
-      );
+      setOriginX((ox) => ox - event.movementX / zoomFactor());
+      setOriginY((oy) => oy - event.movementY / zoomFactor());
     }
   };
 
@@ -73,39 +65,42 @@ export function Canvas() {
     }`;
 
   // Zoom handling
-  function scale(factor: number) {
+  function scale(
+    factor: number,
+    origin: [number, number] = [mouseX(), mouseY()]
+  ) {
     setZoom((v) => v + factor);
 
     const zoomDelta = computeZoom(factor);
     const currentZoom = zoomFactor();
     const targetZoom = currentZoom * zoomDelta;
 
-    const mX = mouseX();
-    const mY = mouseY();
-
+    const [originX, originY] = origin;
     const [dx, dy] = [
-      -mX / targetZoom + mX / currentZoom,
-      -mY / targetZoom + mY / currentZoom,
+      -originX / targetZoom + originX / currentZoom,
+      -originY / targetZoom + originY / currentZoom,
     ];
 
     setOriginX((x) => x + dx);
     setOriginY((y) => y + dy);
   }
 
-  function clampScale(factor: number) {
+  function clampScale(factor: number, origin?: [number, number]) {
     const currentValue = zoom();
     const target = Math.max(
       Math.min(currentValue + factor, MAX_ZOOM),
       MIN_ZOOM
     );
 
-    scale(target - currentValue);
+    scale(target - currentValue, origin);
   }
 
+  // Create event listener for handling mouse zoom
   function onWheel(event: WheelEvent) {
-    clampScale(- event.deltaY * 0.001);
+    clampScale(-event.deltaY * 0.001);
   }
 
+  // Create event handler for overriding browser zoom keys
   function onKeyDown(event: KeyboardEvent) {
     if (event.ctrlKey) {
       switch (event.key) {
@@ -126,17 +121,112 @@ export function Canvas() {
     onCleanup(() => document.removeEventListener("keydown", onKeyDown));
   }
 
+  // Handle touch events for zoom and pan
+  const [trackedTouches, setTrackedTouches] = createSignal<
+    undefined | TouchList
+  >();
+
+  function onTouchStart(event: TouchEvent) {
+    if (event.touches.length) setTrackedTouches(event.touches);
+  }
+
+  function onTouchEnd(event: TouchEvent) {
+    if (!event.touches.length) setTrackedTouches(undefined);
+  }
+
+  function onTouchMove(event: TouchEvent) {
+    const list = trackedTouches();
+    if (!list) return;
+
+    const touchDelta = [...list]
+      .map(
+        (touch) =>
+          [
+            touch,
+            [...event.touches].find(
+              (entry) => entry.identifier === touch.identifier
+            ),
+          ] as [Touch, Touch]
+      )
+      .map(
+        ([start, end]) =>
+          [
+            end.clientX - start.clientX,
+            end.clientY - start.clientY,
+            [end.clientX, end.clientY],
+            [start.clientX, start.clientY],
+          ] as [number, number, [number, number], [number, number]]
+      );
+
+    if (touchDelta.length === 1) {
+      // Pan
+      const [[dx, dy]] = touchDelta;
+      setOriginX((x) => x - dx / zoomFactor());
+      setOriginY((y) => y - dy / zoomFactor());
+    } else if (touchDelta.length === 2) {
+      // Pan and Zoom
+      const [
+        [dx1, dy1, [finalX1, finalY1], [startX1, startY1]],
+        [dx2, dy2, [finalX2, finalY2], [startX2, startY2]],
+      ] = touchDelta;
+
+      // Determine if we are panning or zooming on different x and y axis
+      // equal signs => pan
+      // diff signs => zoom
+      let xScale = 0,
+        yScale = 0;
+
+      if ((dx1 < 0 && dx2 >= 0) || (dx2 < 0 && dx1 >= 0)) {
+        xScale += dx2 - dx1;
+      } else {
+        setOriginX((x) => x - dx1 / zoomFactor());
+      }
+
+      if ((dy1 < 0 && dy2 >= 0) || (dy2 < 0 && dy1 >= 0)) {
+        yScale += dy2 - dy1;
+      } else {
+        setOriginY((x) => x - dx1 / zoomFactor());
+      }
+
+      // Determine scale factor and whether we are zooming
+      let scale = Math.sqrt(xScale ** 2 + yScale ** 2);
+      if (scale) {
+        // Determine direction of zoom
+        let startMagntiude = Math.sqrt(
+          (startX2 - startX1) ** 2 + (startY2 - startY1) ** 2
+        );
+
+        let finalMagntiude = Math.sqrt(
+          (finalX2 - finalX1) ** 2 + (finalY2 - finalY1) ** 2
+        );
+
+        const direction = finalMagntiude - startMagntiude > 0 ? 1 : -1;
+
+        // Find the center point of the zoom operation
+        const origin = [
+          startX1 + (startX2 - startX1) / 2,
+          startY1 + (startY2 - startY1) / 2,
+        ] as [number, number];
+
+        // And zoom
+        clampScale(scale * direction * 0.01, origin);
+      }
+    }
+
+    setTrackedTouches(event.touches);
+  }
+
   return (
     <svg
       class={styles.canvas}
       ref={svgRef}
       viewBox={viewBox()}
-      onMouseDown={startPan}
-      onMouseUp={stopPan}
-      onMouseMove={onPan}
-      onTouchStart={startPan}
-      onTouchEnd={stopPan}
-      onTouchMove={onPan}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+      onMouseMove={onMouseMove}
+      onTouchStart={onTouchStart}
+      onTouchEnd={onTouchEnd}
+      onTouchMove={onTouchMove}
       onWheel={onWheel}
     >
       <foreignObject x={100} y={100} width={100} height={100}>
--- packages/website/src/routes/flows/[id].tsx
+++ packages/website/src/routes/flows/[id].tsx
@@ -1,9 +1,14 @@
+import { Meta } from "solid-start";
 import { Canvas } from "../../../components/Canvas";
 import styles from "./editor.module.css";
 
 export default function Home() {
   return (
     <main class={styles.container}>
+      <Meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, user-scalable=no"
+      />
       <Canvas />
     </main>
   );
