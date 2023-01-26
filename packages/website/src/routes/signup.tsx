265cfe8d6fd98f6209d13cc1d11489c6f53d3705
Cosmo <k21090628@kcl.ac.uk>
2023-01-26 16:05:50 +0000

    feat: signup page

--- packages/website/src/routes/signup.tsx
+++ packages/website/src/routes/signup.tsx
@@ -2,7 +2,7 @@ import {
     Button_Option,
     Button_Primary,
   } from "~/components/inputs/Buttons";
-  import { Email_Textbox, Password_Textbox } from "~/components/inputs/Textbox";
+  import { Email_Textbox, Password_Textbox, Username_Textbox } from "~/components/inputs/Textbox";
   import "./index.css";
   
   export default function Home() {
@@ -12,23 +12,27 @@ import {
             <div class="block pt-6 pr-6 pl-6 pb-4 rounded-lg shadow-lg bg-white max-w-sm bg-slate-800">
             
                 <div class="pb-2">
+                <Username_Textbox id="username" />
+                </div>
+                <div class="py-2">
                 <Email_Textbox id="email" />
                 </div>
                 <div class="py-2">
                 <Password_Textbox id="password">Password</Password_Textbox>
                 </div>
                 <div class="py-2">
+                <Password_Textbox id="password_confirm">Confirm Password</Password_Textbox>
+                </div>
+                <div class="py-2">
                     <div class="flex space-x-2 justify-center">
-                 <a href="/login"> {/*Needs to point to function */}
-                    <Button_Primary>Log in</Button_Primary> 
+                 <a href="/signup"> {/*Needs to point to function */}
+                    <Button_Primary>Sign up</Button_Primary> 
                 </a>
-                <a href="/signup">
-                    <Button_Option>Sign up</Button_Option>
+                <a href="/login">
+                    <Button_Option>Log in</Button_Option>
                 </a>
                 </div>
-                <div class="pt-2">
-                <a class="text-gray-500 text-sm hover:underline transition duration-150 ease-in-out" href="#!">Forgot password?</a>
-                </div>
+
                 </div>
             </div>
         </div>
