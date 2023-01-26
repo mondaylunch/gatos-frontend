5ff004319e103142a86211a3fdefd8ffeab912b9
Cosmo <k21090628@kcl.ac.uk>
2023-01-26 16:55:15 +0000

    feat: landing page

--- packages/website/src/routes/index.tsx
+++ packages/website/src/routes/index.tsx
@@ -9,20 +9,22 @@ import "./index.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Gatos</h1>
-      <div class="flex space-x-2 justify-center">
-        <Button_Success>Success</Button_Success>
-        <Button_Danger>Danger</Button_Danger>
-        <Button_Option>Option</Button_Option>
-        <Button_Primary>Primary</Button_Primary>
-      </div>
-      <div class="flex justify-center">
-        <div class="mb-3 xl:w-96">
-          <Email_Textbox id="email" />
-          <Password_Textbox id="email" />
-        </div>
-      </div>
-    </main>
+<div class="bg-gradient-to-br from-indigo-600 to-teal-500 min-h-screen flex flex-col justify-center items-center">
+  <h1 class="text-3xl font-medium text-center text-white mb-6">Temporary</h1>
+  <div class="w-full max-w-md">
+    <div class="bg-white p-6 rounded-[35px] shadow-md flex justify-center">
+    <a class="w-full mr-2" href="/login">
+    <button class="w-full mr-2 bg-indigo-500 text-white p-3 rounded-[20px] hover:bg-indigo-600">
+          Log in
+        </button>
+    </a>
+      <a class="w-full" href="/signup">
+        <button class="w-full bg-indigo-500 text-white p-3 rounded-[20px] hover:bg-indigo-600">
+          Sign Up
+         </button>
+    </a>
+    </div>
+  </div>
+</div>
   );
 }
