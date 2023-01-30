98d468c1083f6a8fdbd280b41b5de86632f559d2
Paul Makles <me@insrt.uk>
2023-01-30 12:23:50 +0000

    chore: switch to solid icons

--- packages/website/src/components/dashboard/squares.tsx
+++ packages/website/src/components/dashboard/squares.tsx
@@ -1,5 +1,4 @@
-import type { JSX} from "solid-js";
-import "../icons.css";
+import { BsPlus } from 'solid-icons/bs';
 
 type DisplayProps = {
     title: string;
@@ -9,7 +8,7 @@ type DisplayProps = {
 export function Square_New() {
     return (
         <div class="relative h-32 w-32 rounded-[35px] border-dashed border-8 border-neutral-700 bg-transparent flex items-center justify-center hover:scale-110 hover:bg-neutral-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-00 transition duration-150 ease-in-out ">
-                <i class="bi bi-plus text-neutral-500 text-8xl"></i>
+            <BsPlus class="fill-neutral-500 text-8xl" />
         </div>
     )
     }
