d3989cd2f75c30430d3f90a08793d19d1c6701d5
William Lingard <William.Lingard@kcl.ac.uk>
2023-01-30 15:18:21 +0000

    fix: Add form use to signup

--- packages/website/src/components/inputs/Buttons.tsx
+++ packages/website/src/components/inputs/Buttons.tsx
@@ -11,7 +11,7 @@ export function Button_Success(props: Props) {
     <div class="flex space-x-2 justify-center">
       <div>
         <button
-          type="button"
+          type={props.type ?? "button"}
           class={
             (props.class ?? "") +
             "w-full px-6 pt-2.5 pb-2 bg-green-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
@@ -32,7 +32,7 @@ export function Button_Danger(props: Props) {
     <div class="flex space-x-2 justify-center">
       <div>
         <button
-          type="button"
+          type={props.type ?? "button"}
           class={
             (props.class ?? "") +
             "w-full px-6 pt-2.5 pb-2 bg-red-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
@@ -53,7 +53,7 @@ export function Button_Primary(props: Props) {
     <div class="flex space-x-2 justify-center">
       <div>
         <button
-          type="button"
+          type={props.type ?? "button"}
           class={
             (props.class ?? "") +
             "w-full px-6 pt-2.5 pb-2 bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
@@ -74,7 +74,7 @@ export function Button_Option(props: Props) {
     <div class="flex space-x-2 justify-center">
       <div>
         <button
-          type="button"
+          type={props.type ?? "button"}
           class={
             (props.class ?? "") +
             "w-full px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-normal uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
--- packages/website/src/routes/signup.tsx
+++ packages/website/src/routes/signup.tsx
@@ -1,41 +1,44 @@
+import { Button_Option, Button_Primary } from "~/components/inputs/Buttons";
 import {
-    Button_Option,
-    Button_Primary,
-  } from "~/components/inputs/Buttons";
-  import { Email_Textbox, Password_Textbox, Username_Textbox } from "~/components/inputs/Textbox";
-  import "./index.css";
-  
-  export default function Home() {
-    return (
-    
-        <div class="flex h-screen w-screen items-center justify-center bg-neutral-900">
-            <div class="block pt-6 pr-6 pl-6 pb-4 rounded-[35px] shadow-lg max-w-sm bg-neutral-800">
-            
-                <div class="pb-2">
-                <Username_Textbox id="username" />
-                </div>
-                <div class="py-2">
-                <Email_Textbox id="email" />
-                </div>
-                <div class="py-2">
-                <Password_Textbox id="password">Password</Password_Textbox>
-                </div>
-                <div class="py-2">
-                <Password_Textbox id="password_confirm">Confirm Password</Password_Textbox>
-                </div>
-                <div class="py-2">
-                    <div class="flex space-x-2 justify-center">
-                 <a href="/signup"> {/*Needs to point to function */}
-                    <Button_Primary>Sign up</Button_Primary> 
-                </a>
-                <a href="/login">
-                    <Button_Option>Log in</Button_Option>
-                </a>
-                </div>
+  Email_Textbox,
+  Password_Textbox,
+  Username_Textbox,
+} from "~/components/inputs/Textbox";
+import "./index.css";
 
-                </div>
+export default function Home() {
+  return (
+    <div class="flex h-screen w-screen items-center justify-center bg-neutral-900">
+      <div class="block pt-6 pr-6 pl-6 pb-4 rounded-[35px] shadow-lg max-w-sm bg-neutral-800">
+        <form>
+          <div class="pb-2">
+            <Username_Textbox id="username" />
+          </div>
+          <div class="py-2">
+            <Email_Textbox id="email" />
+          </div>
+          <div class="py-2">
+            <Password_Textbox id="password">Password</Password_Textbox>
+          </div>
+          <div class="py-2">
+            <Password_Textbox id="password_confirm">
+              Confirm Password
+            </Password_Textbox>
+          </div>
+          <div class="py-2">
+            <div class="flex space-x-2 justify-center">
+              <a href="/signup">
+                {" "}
+                {/*Needs to point to function */}
+                <Button_Primary class="submit">Sign up</Button_Primary>
+              </a>
+              <a href="/login">
+                <Button_Option>Log in</Button_Option>
+              </a>
             </div>
-        </div>
-    );
-  }
-  
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
