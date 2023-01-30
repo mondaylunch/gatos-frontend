3ad50226ea1b78a5f194fd997fe5c51ad7c5a22c
William Lingard <William.Lingard@kcl.ac.uk>
2023-01-30 13:01:26 +0000

    style: update style of dash box

--- packages/website/src/components/dashboard/squares.tsx
+++ packages/website/src/components/dashboard/squares.tsx
@@ -1,24 +1,29 @@
-import type { JSX} from "solid-js";
+import type { JSX } from "solid-js";
 import "../icons.css";
 
 type DisplayProps = {
-    title: string;
-    description: string
-}
+  title: string;
+  description: string;
+};
 
 export function Square_New() {
-    return (
-        <div class="relative h-32 w-32 rounded-[35px] border-dashed border-8 border-neutral-700 bg-transparent flex items-center justify-center hover:scale-110 hover:bg-neutral-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-00 transition duration-150 ease-in-out ">
-                <i class="bi bi-plus text-neutral-500 text-8xl"></i>
-        </div>
-    )
-    }
+  return (
+    <div class="group relative h-32 w-32 rounded-[35px] border-dashed border-8 border-neutral-700 bg-transparent flex items-center justify-center hover:scale-110 hover:bg-neutral-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-00 transition duration-150 ease-in-out ">
+      <i class="bi bi-plus text-neutral-500 text-8xl group-hover:text-sky-500"></i>
+    </div>
+  );
+}
 
 export function Square_File(props: DisplayProps) {
-    return (
-        <div class="relative h-32 w-32 rounded-[35px] bg-neutral-900 flex items-center justify-center hover:scale-110 hover:bg-neutral-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-00 transition duration-150 ease-in-out ">
-                <p>{props.title}</p>
-                <p>{props.description}</p>
-        </div>
-    )
+  return (
+    <div class="group relative h-32 w-32 rounded-[35px] bg-neutral-900 flex items-center justify-center align-items-center hover:scale-110 hover:bg-neutral-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-00 transition duration-150 ease-in-out">
+      <div class="flex flex-col text-center items-center text-neutral-200 hover:text-neutral-300">
+        <p class="font-bold capitalize group-hover:text-sky-500 transition duration-150 ease-in-out">
+          {props.title}
+        </p>
+        <hr class="border-neutral-500 w-5/6" />
+        <p>{props.description}</p>
+      </div>
+    </div>
+  );
 }
