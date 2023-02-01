0bdbb2015f6c078aa2195d8ccd515fe2a0019f4e
Cat <114757710+TooMuchCat@users.noreply.github.com>
2023-02-01 16:01:16 +0000

    feat: add variable node

--- packages/website/src/components/nodes/Node.tsx
+++ packages/website/src/components/nodes/Node.tsx
@@ -1,8 +1,18 @@
 export function Action_Node() {
     return (
-        <div class="group relative w-72 h-32 rounded-[35px] border-8  bg-white flex items-center justify-center">
+        <div class="group relative w-72 h-32 rounded-[35px] bg-white flex items-center justify-center">
              <p class="font-bold capitalize">
              Concatenate
+        </p>
+        </div>
+    )
+}
+
+export function Variable_Node() {
+    return (
+        <div class="text-white group relative w-28 h-8 rounded-[35px] bg-rose-500 flex items-center justify-center">
+             <p class="font-bold">
+             name
         </p>
         </div>
     )
