9fe039cf00f2c923ce033eb1998c850ef62bccd6
Paul Makles <me@insrt.uk>
2023-02-06 17:24:35 +0000

    feat: add basic cookie session boilerplate

---
+++ packages/website/src/lib/session.ts
@@ -0,0 +1,61 @@
+import { createCookieSessionStorage } from "solid-start";
+
+const ENDPOINT = process.env.API_URL ?? "http://localhost:4390";
+
+type User = {
+  _id: string;
+  username: string;
+  email: string;
+  password: string;
+  auth_token: string;
+};
+
+export function resolveUserByToken(token: string): Promise<User> {
+  return fetch(ENDPOINT + "/api/v1/flows/list", {
+    headers: {
+      "x-auth-token": token,
+    },
+  }).then((x) => x.json());
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [process.env.SESSION_SECRET! ?? "joigfgjhogfjhgfjd"],
+    sameSite: "strict",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export function login(data: Pick<User, "email" | "password">): Promise<User> {
+  return fetch(ENDPOINT + "/api/v1/login/authenticate", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((x) => x.json());
+}
+
+export function register(
+  data: Pick<User, "username" | "email" | "password">
+): Promise<User> {
+  return fetch(ENDPOINT + "/api/v1/sign_up", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((x) => x.json());
+}
+
+// Expose globals
+if (typeof window !== "undefined") {
+  (window as any).auth = {
+    login,
+    register,
+  };
+}
