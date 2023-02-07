9e33dd8e2d5307e1c71edb5d42f7d44f8d0b50ef
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-02-07 14:01:22 +0000

    test: basic test skeleton + file structure created

--- packages/playwright/__tests__/it-works.spec.ts
+++ packages/playwright/__tests__/it-works.spec.ts
@@ -5,6 +5,18 @@ test("it loads the Homepage", async ({ page }) => {
   await expect(page).toHaveScreenshot();
 });
 
+test("page has correct title", async ({ page }) => {
+  await page.goto("http://localhost:5173");
+  await expect(page).toHaveTitle("Gatos")
+});
+
+test("redirect to login page", async ({ page }) => {
+  await page.goto("http://localhost:5173");
+  await page.getByRole("button", {name: "Log In"}).click();
+  //http://localhost:5173/login
+  await expect(page).toHaveURL("http://localhost:5173/login")
+});
+
 test("it loads the Editor", async ({ page }) => {
   await page.goto("http://localhost:5173/flows/1");
   await expect(page).toHaveScreenshot();
