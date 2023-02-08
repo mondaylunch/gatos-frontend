7a6e201cafa37de31be5a978b8d93ec126592caa
Paul Makles <me@insrt.uk>
2023-02-08 21:04:21 +0000

    feat(auth): use route data to fetch user info

--- packages/website/src/routes/dash.tsx
+++ packages/website/src/routes/dash.tsx
@@ -1,24 +1,30 @@
-import { createEffect } from "solid-js";
-import {
-  Square_New,
-  Square_File
-} from "~/components/dashboard/squares";
-import { username } from "~/components/forms/useLoginForm";
+import { useRouteData } from "solid-start";
+import { createServerData$ } from "solid-start/server";
+import { Square_New, Square_File } from "~/components/dashboard/squares";
+import { resolveUserByRouteEvent } from "~/lib/session";
+
+export function routeData() {
+  return createServerData$(async (_, event) => {
+    return {
+      user: await resolveUserByRouteEvent(event),
+    };
+  });
+}
 
 export default function Home() {
-  createEffect(() => alert(username()));
+  const data = useRouteData<typeof routeData>();
 
   return (
-      <div class="flex flex-col items-center justify-center w-screen h-screen bg-neutral-800">
-          <div class="absolute top-0 right-0 mr-5 mt-5">
-            <p class="text-neutral-200 font-medium">Hi {username()}</p>
-          </div>
-        <div class="flex">
-          <div class="mr-5">
-          <Square_New />
-          </div>
-          <Square_File title="Title of me" description="Look at my description" />
-        </div>
+    <div class="flex flex-col items-center justify-center w-screen h-screen bg-neutral-800">
+      <div class="absolute top-0 right-0 mr-5 mt-5">
+        <p class="text-neutral-200 font-medium">Hi {data()?.user.username}</p>
       </div>
+      <div class="flex">
+        <div class="mr-5">
+          <Square_New />
+        </div>
+        <Square_File title="Title of me" description="Look at my description" />
+      </div>
+    </div>
   );
 }
