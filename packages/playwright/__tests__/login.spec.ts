e4cbf625e4261bd652efe87896870a14698918f9
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-02-08 13:25:36 +0000

    chore: add beforeEach method to reduce code duplication

---
+++ packages/playwright/__tests__/login.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "@playwright/test";
+
+const URL = "http://localhost:5173/login";
+
+test.beforeEach(async ({page}, testInfo) => {
+    await page.goto(URL);
+});
+
+test("page has two buttons", async ({ page }) => {
+    const all_buttons = await page.locator("button").count();
+    expect(all_buttons).toBe(2);
+});
+
+test("page has 4 fields", async ({ page }) => {
+    const all_fields = await page.locator("input").count();
+    expect(all_fields).toBe(2);
+});
--- packages/playwright/__tests__/main-page.spec.ts
+++ packages/playwright/__tests__/main-page.spec.ts
@@ -1,23 +1,30 @@
 import { expect, test } from "@playwright/test";
 
+const URL = "http://localhost:5173/";
+
+test.beforeEach(async ({page}, testInfo) => {
+    await page.goto(URL);
+});
+
 test("it loads the Homepage", async ({ page }) => {
-    await page.goto("http://localhost:5173");
     await expect(page).toHaveScreenshot();
 });
 
 test("page has correct title", async ({ page }) => {
-    await page.goto("http://localhost:5173");
     await expect(page).toHaveTitle("Gatos")
 });
 
 test("redirect to login page", async ({ page }) => {
-    await page.goto("http://localhost:5173");
     await page.getByRole("button", {name: "Log in"}).click();
     await expect(page).toHaveURL("http://localhost:5173/login")
 });
 
 test("redirect to sign up page", async ({ page }) => {
-    await page.goto("http://localhost:5173");
     await page.getByRole("button", {name: "Sign up"}).click();
     await expect(page).toHaveURL("http://localhost:5173/signup")
+});
+
+test("page has two buttons", async ({ page }) => {
+    const all_buttons = await page.locator("button").count();
+    expect(all_buttons).toBe(2);
 });
