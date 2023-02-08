bc7e0ef9632d944eabf8169f3f51fe12356ff159
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-02-08 12:37:54 +0000

    refactor(tests): remove duplicate tests

--- packages/playwright/__tests__/it-works.spec.ts
+++ packages/playwright/__tests__/it-works.spec.ts
@@ -1,10 +1,5 @@
 import { expect, test } from "@playwright/test";
 
-test("it loads the Homepage", async ({ page }) => {
-  await page.goto("http://localhost:5173");
-  await expect(page).toHaveScreenshot();
-});
-
 test("it loads the Editor", async ({ page }) => {
   await page.goto("http://localhost:5173/flows/1");
   await expect(page).toHaveScreenshot();
