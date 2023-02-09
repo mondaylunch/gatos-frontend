c6315a7757a097993857088cdffb35c77c10de0c
Paul Makles <me@insrt.uk>
2023-02-09 14:22:12 +0000

    chore(editor): remove margin on nodes

--- packages/website/src/components/nodes/Node.tsx
+++ packages/website/src/components/nodes/Node.tsx
@@ -1,9 +1,13 @@
-export function Action_Node() {
+import { JSX } from "solid-js";
+
+export function Action_Node(props: { children?: JSX.Element }) {
   return (
     <div class="group relative h-32 w-72 rounded-[35px] bg-white flex align-text-top items-center justify-center align-items-center">
       <div class="flex flex-col text-center items-center text-black">
         <p class="font-bold capitalize select-none">Concatenate</p>
-        <div class="flex-col w-60 h-16 rounded-[35px] bg-neutral-800 flex align-text-top justify-center"></div>
+        <div class="flex-col w-60 h-16 rounded-[35px] bg-neutral-800 flex align-text-top justify-center">
+          {props.children}
+        </div>
       </div>
     </div>
   );
@@ -19,7 +23,7 @@ export function Variable_Node() {
 
 export function Start_Node() {
   return (
-    <div class="rounded-t-full h-32 w-64 flex bg-neutral-900 m-2 flex items-center justify-center">
+    <div class="rounded-t-full h-32 w-64 flex bg-neutral-900 flex items-center justify-center">
       <p class="font-bold text-white select-none">HTTP request</p>
     </div>
   );
@@ -27,7 +31,7 @@ export function Start_Node() {
 
 export function End_Node() {
   return (
-    <div class="rounded-b-full h-32 w-64 flex bg-neutral-900 m-2 flex items-center justify-center">
+    <div class="rounded-b-full h-32 w-64 flex bg-neutral-900 flex items-center justify-center">
       <p class="font-bold text-white select-none">OUTPUT</p>
     </div>
   );
