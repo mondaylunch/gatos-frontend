1db29383247a5045c3a50a91ead2821aac8ae9cd
Paul Makles <me@insrt.uk>
2023-02-25 09:59:40 +0000

    chore: start work on settings typings

--- packages/website/src/lib/types.ts
+++ packages/website/src/lib/types.ts
@@ -6,16 +6,21 @@ export type User = {
   authToken: string;
 };
 
+export type Setting = {
+  type: `option$${"test" | "gfgdf"}`;
+  value: any;
+};
+
 export type Node = {
   id: string;
   type: string;
-  settings: Record<string, string>;
+  settings: Record<string, Setting>;
 };
 
 export type Connector = {
   nodeId: string;
   name: string;
-  type: "integer";
+  type: "number";
 };
 
 export type Connection = {
@@ -62,58 +67,83 @@ export const NODE_TYPES = {
 
 export const SAMPLE_FLOW_DATA: Flow = {
   _id: "269e8442-05b9-4981-a9cb-c780a42cba30",
+  name: "Test Flow",
+  description: "This is a test flow",
   author_id: "166d8840-322d-4d9e-9967-cb5be6d49af2",
-  description: "",
   graph: {
     nodes: [
       {
-        id: "6f8de627-706d-4817-8921-73bff23006a8",
-        type: "test_process",
-        settings: {},
-      },
-      {
         id: "9f60cd6b-b4c2-43a1-83b7-711aa90ce8fd",
         type: "test_start",
-        settings: {},
+        settings: {
+          setting: {
+            type: "number",
+            value: 0.0,
+          },
+        },
+        inputTypes: {},
+      },
+      {
+        id: "6f8de627-706d-4817-8921-73bff23006a8",
+        type: "test_process",
+        settings: {
+          setting: {
+            type: "list$number",
+            value: [20.0, 3.0],
+          },
+        },
+        inputTypes: {
+          process_input: "number",
+        },
       },
       {
         id: "b15f484f-4345-4f30-9162-5210b4ff1433",
         type: "test_end",
-        settings: {},
+        settings: {
+          setting: {
+            type: "optional$number",
+            value: {
+              present: true,
+              value: 1.5,
+            },
+          },
+        },
+        inputTypes: {
+          end_input: "number",
+        },
       },
     ],
     connections: [
       {
-        input: {
-          nodeId: "b15f484f-4345-4f30-9162-5210b4ff1433",
-          name: "in",
-          type: "integer",
-        },
         output: {
+          nodeId: "9f60cd6b-b4c2-43a1-83b7-711aa90ce8fd",
+          name: "start_output",
+          type: "number",
+        },
+        input: {
           nodeId: "6f8de627-706d-4817-8921-73bff23006a8",
-          name: "out",
-          type: "integer",
+          name: "process_input",
+          type: "number",
         },
       },
       {
-        input: {
-          nodeId: "6f8de627-706d-4817-8921-73bff23006a8",
-          name: "in",
-          type: "integer",
-        },
         output: {
-          nodeId: "9f60cd6b-b4c2-43a1-83b7-711aa90ce8fd",
-          name: "out",
-          type: "integer",
+          nodeId: "6f8de627-706d-4817-8921-73bff23006a8",
+          name: "process_output",
+          type: "number",
+        },
+        input: {
+          nodeId: "b15f484f-4345-4f30-9162-5210b4ff1433",
+          name: "end_input",
+          type: "number",
         },
       },
     ],
     metadata: {
       "9f60cd6b-b4c2-43a1-83b7-711aa90ce8fd": {
-        xPos: 100.0,
+        xPos: 1.0,
         yPos: 0.0,
       },
     },
   },
-  name: "Test Flow",
 };
