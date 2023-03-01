0468fcdd9c056e653ca682cd0a44af2a1bf8ef18
Paul Makles <me@insrt.uk>
2023-03-01 16:04:04 +0000

    feat: calculate and cache element size on canvas

--- packages/website/src/components/editor/CanvasElement.tsx
+++ packages/website/src/components/editor/CanvasElement.tsx
@@ -1,4 +1,5 @@
-import { JSX, splitProps } from "solid-js";
+import { createEffect, JSX, on, splitProps, useContext } from "solid-js";
+import { ElementSizeContext, ZoomFactorContext } from "./Canvas";
 
 type Props = Omit<
   JSX.ForeignObjectSVGAttributes<SVGForeignObjectElement>,
@@ -18,16 +19,65 @@ type Props = Omit<
    * Child elements
    */
   children: JSX.Element;
+
+  /**
+   * Static unique ID of this canvas element
+   * Specify this if you want its width / height cached and accessible
+   */
+  id?: string;
 };
 
 /**
  * Wrapper element for rendering into the canvas
  */
 export function CanvasElement(props: Props) {
-  const [local, remote] = splitProps(props, ["x", "y"]);
+  const [local, remote] = splitProps(props, ["x", "y", "id"]);
+
+  // Declare variable as our reference
+  let ref: SVGForeignObjectElement | undefined;
+
+  // If an id has been specified, start monitoring child element
+  if (local.id) {
+    const [_, setSize] = useContext(ElementSizeContext)!;
+    const zoomFactor = useContext(ZoomFactorContext)!;
+
+    createEffect(
+      on(
+        () => ref,
+        (ref) => {
+          if (ref) {
+            // Pull out first child
+            const firstChild = ref.childNodes[0]?.childNodes[0];
+
+            // Calculate and commit size
+            if (firstChild) {
+              function commit() {
+                const rect = (
+                  firstChild as HTMLElement
+                ).getBoundingClientRect();
+
+                const width = rect.width / zoomFactor();
+                const height = rect.height / zoomFactor();
+
+                setSize(local.id!, { width, height });
+              }
+
+              commit();
+
+              const observer = new ResizeObserver(commit);
+              observer.observe(firstChild as HTMLElement);
+              return () => observer.disconnect();
+            }
+          }
+        }
+      )
+    );
+  }
+
   return (
     <foreignObject
       {...remote}
+      ref={ref}
       x={local.x}
       y={local.y}
       style={{
--- packages/website/src/components/nodes/RenderConnections.tsx
+++ packages/website/src/components/nodes/RenderConnections.tsx
@@ -1,10 +1,13 @@
-import { For } from "solid-js";
 import { Graph } from "~/lib/types";
+import { For, useContext } from "solid-js";
+import { ElementSizeContext } from "../editor/Canvas";
 
 /**
  * Draw line connections between nodes
  */
 export function RenderConnections(props: { graph: Graph }) {
+  const [elementSizeCache] = useContext(ElementSizeContext)!;
+
   return (
     <For each={props.graph.connections}>
       {(connection, index) => {
@@ -12,13 +15,24 @@ export function RenderConnections(props: { graph: Graph }) {
         const outputMetadata = props.graph.metadata[connection.output.nodeId];
         const inputMetadata = props.graph.metadata[connection.input.nodeId];
 
+        // Get size of output and input nodes.
+        const outputRect = () =>
+          elementSizeCache[connection.output.nodeId] ?? {};
+        const inputRect = () => elementSizeCache[connection.input.nodeId] ?? {};
+
         // TODO: the code below needs to be replaced with line drawing code,
         // the line must be drawn from (output.x,y) to (input.x,y)
         return (
           <text
             x={outputMetadata.xPos}
             y={outputMetadata.yPos + 180 + index() * 20}
-            textContent={`draw line connection from (${outputMetadata.xPos}, ${outputMetadata.yPos}) to (${inputMetadata.xPos}, ${inputMetadata.yPos})`}
+            textContent={`draw line connection from (${outputMetadata.xPos}, ${
+              outputMetadata.yPos
+            }) [${outputRect().width}, ${outputRect().height}] to (${
+              inputMetadata.xPos
+            }, ${inputMetadata.yPos}) [${inputRect().width}, ${
+              inputRect().height
+            }]`}
             fill="white"
           />
         );
