c262aaa2d399cf86600dc600eeb2706d24583e05
Paul Makles <me@insrt.uk>
2023-03-05 08:57:33 +0000

    fix: render lines below nodes

--- packages/website/src/routes/flows/[id].tsx
+++ packages/website/src/routes/flows/[id].tsx
@@ -142,8 +142,8 @@ export default function FlowEditor() {
       handleDrop={handleDrop}
       renderVirtualElement={renderVirtualElement}
     >
-      <RenderNodes graph={graph} />
       <RenderConnections graph={graph} />
+      <RenderNodes graph={graph} />
     </InteractiveCanvas>
   );
 }
