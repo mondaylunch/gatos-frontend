fb1ea38ebd7f8d0205030255155b304fc4f1d421
Paul Makles <me@insrt.uk>
2023-03-05 08:58:12 +0000

    feat: navigate to owned flows feat: create new flows

--- packages/website/src/routes/dash.tsx
+++ packages/website/src/routes/dash.tsx
@@ -1,12 +1,12 @@
 import { For } from "solid-js";
 import { ENDPOINT } from "~/lib/env";
-import { useRouteData } from "solid-start";
+import { A, useNavigate, useRouteData } from "solid-start";
 import { createServerData$ } from "solid-start/server";
 import { resolveUserByRouteEvent } from "~/lib/session";
 import { Square_New, Square_File } from "~/components/dashboard/squares";
 
 type Flow = {
-  id: string;
+  _id: string;
   name: string;
   description: string;
   author_id: string;
@@ -30,6 +30,25 @@ export function routeData() {
 
 export default function Dash() {
   const data = useRouteData<typeof routeData>();
+  const navigate = useNavigate();
+
+  async function createFlow() {
+    const name = prompt("Enter flow name:");
+    if (name) {
+      await fetch(`${ENDPOINT}/api/v1/flows`, {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          "X-Auth-Token": data()!.user.auth_token,
+        },
+      })
+        .then((res) => res.json())
+        .then((flow) => navigate(`/flows/${flow._id}`));
+    }
+  }
 
   return (
     <div class="flex flex-col items-center justify-center w-screen h-screen bg-neutral-800">
@@ -37,10 +56,14 @@ export default function Dash() {
         <p class="text-neutral-200 font-medium">Hi {data()?.user.username}</p>
       </div>
       <div class="grid grid-cols-4 gap-5">
-        <Square_New />
+        <a class="cursor-pointer" onClick={createFlow}>
+          <Square_New />
+        </a>
         <For each={data()?.flows ?? []}>
           {(flow) => (
-            <Square_File title={flow.name} description={flow.description} />
+            <A href={`/flows/${flow._id}`}>
+              <Square_File title={flow.name} description={flow.description} />
+            </A>
           )}
         </For>
       </div>
