7b17348053fd56b8090fe5a5170b12e8110fcefc
Cat <114757710+TooMuchCat@users.noreply.github.com>
2023-03-08 16:37:52 +0000

    feat: Add navbar to editor

--- packages/website/src/routes/flows/[id].tsx
+++ packages/website/src/routes/flows/[id].tsx
@@ -1,43 +1,47 @@
-import { useRouteData } from "solid-start";
+import {useRouteData} from "solid-start";
 
-import { Flow, NodeType } from "~/lib/types";
-import { createServerData$ } from "solid-start/server";
-import { MountUser, resolveUserByRouteEvent, setUser } from "~/lib/session";
-import { ENDPOINT } from "~/lib/env";
-import { Show } from "solid-js";
-import { FlowEditor } from "~/components/nodes/FlowEditor";
+import {Flow, NodeType} from "~/lib/types";
+import {createServerData$} from "solid-start/server";
+import {MountUser, resolveUserByRouteEvent, setUser} from "~/lib/session";
+import {ENDPOINT} from "~/lib/env";
+import {Show} from "solid-js";
+import {FlowEditor} from "~/components/nodes/FlowEditor";
+import {Navbar} from "~/components/shared/Navbar";
 
 export function routeData() {
-  return createServerData$(async (_, event) => {
-    const user = await resolveUserByRouteEvent(event);
+    return createServerData$(async (_, event) => {
+        const user = await resolveUserByRouteEvent(event);
 
-    // Route data doesn't provide params because Router is
-    // inaccessible, so we just read the URL instead, which
-    // is good enough for our purposes.
-    const flowId = event.request.url.split("/").pop()!;
+        // Route data doesn't provide params because Router is
+        // inaccessible, so we just read the URL instead, which
+        // is good enough for our purposes.
+        const flowId = event.request.url.split("/").pop()!;
 
-    return {
-      user,
-      flow: await fetch(`${ENDPOINT}/api/v1/flows/${flowId}`, {
-        method: "GET",
-        headers: {
-          "X-Auth-Token": user.auth_token,
-        },
-      }).then((res) => res.json() as Promise<Flow>),
-      nodeTypes: await fetch(`${ENDPOINT}/api/v1/node-types`).then(
-        (res) => res.json() as Promise<NodeType[]>
-      ),
-    };
-  });
+        return {
+            user,
+            flow: await fetch(`${ENDPOINT}/api/v1/flows/${flowId}`, {
+                method: "GET",
+                headers: {
+                    "X-Auth-Token": user.auth_token,
+                },
+            }).then((res) => res.json() as Promise<Flow>),
+            nodeTypes: await fetch(`${ENDPOINT}/api/v1/node-types`).then(
+                (res) => res.json() as Promise<NodeType[]>
+            ),
+        };
+    });
 }
 
 export default function FlowPage() {
-  const data = useRouteData<typeof routeData>();
+    const data = useRouteData<typeof routeData>();
 
-  return (
-    <Show when={data()}>
-      <MountUser user={data()!.user} />
-      <FlowEditor flow={data()!.flow} nodeTypes={data()!.nodeTypes} />
-    </Show>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <Show when={data()}>
+                <MountUser user={data()!.user}/>
+                <FlowEditor flow={data()!.flow} nodeTypes={data()!.nodeTypes}/>
+            </Show>
+        </div>
+    );
 }
