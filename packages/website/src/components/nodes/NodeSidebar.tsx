cf4ca3b336b7a8621c33fb321ee8a0c5ee8bdc58
Cat <114757710+TooMuchCat@users.noreply.github.com>
2023-03-08 19:21:55 +0000

    feat: Move draggable element to component

--- packages/website/src/components/nodes/NodeSidebar.tsx
+++ packages/website/src/components/nodes/NodeSidebar.tsx
@@ -3,17 +3,8 @@ import {ProcessNode} from "./Node";
 import {createServerData$} from "solid-start/server";
 import {ENDPOINT} from "~/lib/env";
 import {createSignal, For, Match, Switch} from "solid-js";
-import {
-    FaSolidArrowsLeftRight,
-    FaSolidBox,
-    FaSolidCalculator, FaSolidEquals,
-    FaSolidGreaterThanEqual,
-    FaSolidMagnifyingGlass, FaSolidSkullCrossbones, FaSolidToggleOn,
-    FaSolidUpRightFromSquare, FaSolidCircleExclamation, FaSolidArrowRight,
-    FaSolidArrowLeft, FaSolidQuoteLeft, FaSolidDownLeftAndUpRightToCenter,
-    FaSolidSignsPost, FaSolidRuler, FaSolidCircleCheck, FaSolidTriangleExclamation,
-    FaSolidGlobe, FaSolidCircleQuestion
-} from "solid-icons/fa";
+import {NodeTypeDrag} from "~/components/editor/NodeTypeDrag";
+
 
 grabSource;
 
@@ -24,27 +15,6 @@ type NodeType = {
     category: string;
 };
 
-const Icons_Dict = {
-    "list_length": FaSolidArrowsLeftRight,
-    'variable_extraction': FaSolidUpRightFromSquare,
-    "string_interpolation": FaSolidQuoteLeft,
-    "http_request": FaSolidGlobe,
-    "is_nan": FaSolidTriangleExclamation,
-    "truthiness": FaSolidCircleCheck,
-    "variable_remapping": FaSolidSignsPost,
-    "string_length": FaSolidRuler,
-    "string_concat": FaSolidDownLeftAndUpRightToCenter,
-    "list_head_separation": FaSolidArrowLeft,
-    "is_finite": FaSolidSkullCrossbones,
-    "list_tail_separation": FaSolidArrowRight,
-    "negation": FaSolidCircleExclamation,
-    "equals": FaSolidEquals,
-    "string_contains": FaSolidMagnifyingGlass,
-    "number_comparison": FaSolidGreaterThanEqual,
-    "math": FaSolidCalculator,
-    "boolean_operation": FaSolidToggleOn,
-    "optional_or_else": FaSolidBox,
-}
 
 export function routeData() {
     return createServerData$(async (_, event) => {
@@ -75,27 +45,10 @@ export function NodeSidebar() {
             <div class="flex flex-col gap-2 pl-2 pr-2">
                 <For each={nodeTypes()}>
                     {(nodeType) => {
-                        const Icon = Icons_Dict[nodeType.name as keyof typeof Icons_Dict];
+
                         // @ts-expect-error directives are not supported
                         return <div use:grabSource={{type: "NodeType", node: nodeType}}>
-                            <div
-                                class="flex text-w-full rounded-[30px] border-4 place-content-center bg-white justify-items-center items-center gap-2 pl-1">
-                                <Switch fallback={
-                                    <div>
-                                        <FaSolidCircleQuestion size={20} fill="red-700"/>
-                                        <p class="font-bold flex-grow">
-                                            {nodeType.name}
-                                        </p>
-                                    </div>
-                                }>
-                                    <Match when={nodeType.name in Icons_Dict}>
-                                        <Icon size={20}/>
-                                        <p class="font-bold flex-grow">
-                                            {nodeType.name}
-                                        </p>
-                                    </Match>
-                                </Switch>
-                            </div>
+                            <NodeTypeDrag name={nodeType.name}/>
                         </div>
                     }}
                 </For>
