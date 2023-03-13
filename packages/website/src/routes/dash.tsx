9216cc4505db2724d41327c3c4c0186755523cdb
William Lingard <William.Lingard@kcl.ac.uk>
2023-03-13 20:40:59 +0000

    fix: Allow reset of state

--- packages/website/src/routes/dash.tsx
+++ packages/website/src/routes/dash.tsx
@@ -132,7 +132,10 @@ export default function Dash() {
                     </button>
                     <button
                       class="bg-gray-300 hover:bg-gray-400 text=gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
-                      onClick={() => setShowModal(false)}
+                      onClick={() => {
+                        setShowModal(false);
+                        setSubmitted(false);
+                      }}
                     >
                       Cancel
                     </button>
