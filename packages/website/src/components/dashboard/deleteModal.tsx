13fa3e538bbd67c882851fb6f0648d1a9ad0fafa
William Lingard <William.Lingard@kcl.ac.uk>
2023-03-13 22:31:35 +0000

    fix: Make text readable

--- packages/website/src/components/dashboard/deleteModal.tsx
+++ packages/website/src/components/dashboard/deleteModal.tsx
@@ -22,9 +22,12 @@ export function deleteModal(
     <div class="fixed inset-0 flex items-center justify-center flex-col bg-gray-500 bg-opacity-75">
       <div class="bg-neutral-800 rounded-lg p-8 max-w-xs mx-auto outline outline-indigo-600 outline-offset-2">
         <FaSolidSquareXmark size={48} color="#dc2626" />
-        <div class="text-2xl font-bold mb-4 text-white">Delete Flow:</div>
+        <div class="text-2xl font-bold mb-2 text-white">Delete Flow:</div>
         <div class="mb-4">
-          <p> Are you sure you want to delete: {delTitle}?</p>
+          <p class="text-white pb-2">
+            {" "}
+            Are you sure you want to delete: {delTitle}?
+          </p>
           <div class="flex justify-end">
             <button
               class="flex-auto bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
