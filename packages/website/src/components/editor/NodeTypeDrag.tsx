2dc4deda465e1311561d89d50e2c46466f779487
William Lingard <William.Lingard@kcl.ac.uk>
2023-03-14 22:22:27 +0000

    feat: Move icons to own file

--- packages/website/src/components/editor/NodeTypeDrag.tsx
+++ packages/website/src/components/editor/NodeTypeDrag.tsx
@@ -1,65 +1,6 @@
-import type { JSX } from "solid-js";
 import { Match, Switch } from "solid-js";
-import {
-  FaSolidArrowsLeftRight,
-  FaSolidBox,
-  FaSolidCalculator,
-  FaSolidEquals,
-  FaSolidGreaterThanEqual,
-  FaSolidMagnifyingGlass,
-  FaSolidSkullCrossbones,
-  FaSolidToggleOn,
-  FaSolidUpRightFromSquare,
-  FaSolidCircleExclamation,
-  FaSolidArrowRight,
-  FaSolidArrowLeft,
-  FaSolidQuoteLeft,
-  FaSolidDownLeftAndUpRightToCenter,
-  FaSolidSignsPost,
-  FaSolidRuler,
-  FaSolidCircleCheck,
-  FaSolidTriangleExclamation,
-  FaSolidGlobe,
-  FaSolidCircleQuestion,
-  FaSolidListOl,
-  FaSolidMattressPillow,
-  FaSolidKeyboard,
-  FaSolidStrikethrough,
-  FaSolidHashtag,
-  FaSolidShuffle,
-  FaSolidArrowRightToBracket,
-  FaSolidArrowRightFromBracket,
-} from "solid-icons/fa";
-
-const Icons_Dict = {
-  list_length: FaSolidArrowsLeftRight,
-  variable_extraction: FaSolidUpRightFromSquare,
-  string_interpolation: FaSolidQuoteLeft,
-  http_request: FaSolidGlobe,
-  is_nan: FaSolidTriangleExclamation,
-  truthiness: FaSolidCircleCheck,
-  variable_remapping: FaSolidSignsPost,
-  string_length: FaSolidRuler,
-  string_concat: FaSolidDownLeftAndUpRightToCenter,
-  list_head_separation: FaSolidArrowLeft,
-  is_finite: FaSolidSkullCrossbones,
-  list_tail_separation: FaSolidArrowRight,
-  negation: FaSolidCircleExclamation,
-  equals: FaSolidEquals,
-  string_contains: FaSolidMagnifyingGlass,
-  number_comparison: FaSolidGreaterThanEqual,
-  math: FaSolidCalculator,
-  boolean_operation: FaSolidToggleOn,
-  optional_or_else: FaSolidBox,
-  ["value_provider_Optional[number]"]: FaSolidListOl,
-  ["value_provider_Optional[boolean]"]: FaSolidMattressPillow,
-  ["value_provider_Optional[string]"]: FaSolidKeyboard,
-  ["value_replacer_Optional[string]"]: FaSolidStrikethrough,
-  ["value_replacer_Optional[number]"]: FaSolidHashtag,
-  ["value_replacer_Optional[boolean]"]: FaSolidShuffle,
-  webhook_start: FaSolidArrowRightToBracket,
-  webhook_end: FaSolidArrowRightFromBracket,
-};
+import { FaSolidCircleQuestion } from "solid-icons/fa";
+import { Icons_Dict } from "../shared/NodeIcons";
 
 export function NodeTypeDrag(props: { name: string }) {
   const Icon = Icons_Dict[props.name as keyof typeof Icons_Dict];
