ddae22661867d61552ac9ca7ed20b92be3d37b8f
William Lingard <William.Lingard@kcl.ac.uk>
2023-03-16 19:29:28 +0000

    feat: Change handling of new nodes

--- packages/website/src/components/editor/NodeTypeDrag.tsx
+++ packages/website/src/components/editor/NodeTypeDrag.tsx
@@ -1,24 +1,13 @@
-import { Match, Switch } from "solid-js";
 import { FaSolidCircleQuestion } from "solid-icons/fa";
 import { Icons_Dict } from "../shared/NodeIcons";
 
 export function NodeTypeDrag(props: { name: string }) {
-  const Icon = Icons_Dict[props.name as keyof typeof Icons_Dict];
+  const Icon =
+    Icons_Dict[props.name as keyof typeof Icons_Dict] || FaSolidCircleQuestion;
   return (
     <div class="flex text-w-full rounded-[30px] border-4 place-content-center bg-white justify-items-center items-center gap-2 pl-1 overflow-hidden">
-      <Switch
-        fallback={
-          <div>
-            <FaSolidCircleQuestion size={20} fill="red-700" />
-            <p class="font-bold flex-grow">{props.name}</p>
-          </div>
-        }
-      >
-        <Match when={props.name in Icons_Dict}>
-          <Icon size={20} />
-          <p class="font-bold flex-grow">{props.name}</p>
-        </Match>
-      </Switch>
+      <Icon size={20} />
+      <p class="font-bold flex-grow">{props.name}</p>
     </div>
   );
 }
