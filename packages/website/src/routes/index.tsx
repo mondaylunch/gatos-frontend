eb6fe5895011113e0557bf088074f323830ee68e
Cat <114757710+TooMuchCat@users.noreply.github.com>
2023-03-19 18:21:06 +0000

    feat: change colour scheme

--- packages/website/src/routes/index.tsx
+++ packages/website/src/routes/index.tsx
@@ -8,10 +8,17 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <div class="bg-gradient-to-br from-indigo-600 to-teal-500 min-h-screen flex flex-col justify-center items-center">
-        <h1 class="text-3xl font-medium text-center text-white mb-6">
-          Temporary
-        </h1>
+      <div class="bg-neutral-900 min-h-screen flex flex-col justify-center items-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-neutral-800 to-black">
+        <span class="text-6xl font-semibold mb-1 text-white">
+            Welcome to
+          </span>
+        <span class="text-9xl font-semibold mb-20 text-blue-500" style="background: linear-gradient(-45deg, #eca100, #ec540d, #f44653, #4d58c7); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">
+            GATOS
+          </span>
+        <div class="gradient-text text-4xl font-bold">
+
+
+        </div>
         <div class="w-full max-w-md">
           <div class="bg-white p-6 rounded-[35px] shadow-md flex justify-center">
             <button
