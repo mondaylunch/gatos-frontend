c70b73ef0577775d2f184b70fc50c3141e08a94c
Paul Makles <me@insrt.uk>
2023-03-19 16:41:54 +0000

    chore: further changes to auth

--- packages/website/src/lib/env.ts
+++ packages/website/src/lib/env.ts
@@ -6,16 +6,19 @@ export const ENDPOINT = process.env.API_URL ?? "http://localhost:8080";
 /**
  * Whether the application is currently running in production
  */
-export const PRODUCTION = process.NODE_ENV === "production";
+export const PRODUCTION = import.meta.env.PROD;
 
 /**
  * Secret used to encrypt session storage
  */
 export const AUTH_SECRET =
-  process.env.AUTH_SECRET! ?? "joigfgjhogfjhgjksaghdsfjklgdsfjklgsdfjd";
+  process.env.AUTH_SECRET! ??
+  "e4e11a548656af6d8b1e4edf36a5d06acca47a03035e9fd9eb833cf37e18bd9b";
 
-export const AUTH0_ID = process.env.VITE_AUTH0_ID ?? "";
-export const AUTH0_SECRET = process.env.AUTH0_SECRET ?? "";
-export const AUTH0_ISSUER = process.env.VITE_AUTH0_ISSUER ?? "";
+export const AUTH0_ID = process.env.AUTH0_CLIENT_ID ?? "";
+export const AUTH0_AUTHORIZATION_LINK =
+  process.env.AUTH0_AUTHORIZATION_LINK ?? "";
+export const AUTH0_SECRET = process.env.AUTH0_CLIENT_SECRET ?? "";
+export const AUTH0_ISSUER = process.env.AUTH0_ISSUER ?? "";
 export const AUTH0_TOKEN_URL = process.env.AUTH0_TOKEN_URL ?? "";
 export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE ?? "";
--- packages/website/src/routes/api/auth/[...solidauth].tsx
+++ packages/website/src/routes/api/auth/[...solidauth].tsx
@@ -1,6 +1,12 @@
 import { SolidAuth, type SolidAuthConfig } from "@auth/solid-start";
 import Auth0 from "@auth/core/providers/auth0";
-import { AUTH0_ID, AUTH0_ISSUER, AUTH0_SECRET, AUTH_SECRET } from "~/lib/env";
+import {
+  AUTH0_AUTHORIZATION_LINK,
+  AUTH0_ID,
+  AUTH0_ISSUER,
+  AUTH0_SECRET,
+  AUTH_SECRET,
+} from "~/lib/env";
 
 export const authOpts: SolidAuthConfig = {
   providers: [
@@ -9,11 +15,12 @@ export const authOpts: SolidAuthConfig = {
       clientId: AUTH0_ID,
       clientSecret: AUTH0_SECRET,
       issuer: AUTH0_ISSUER,
-      authorization: "https://mondaylunchdev.eu.auth0.com/authorize",
+      authorization: AUTH0_AUTHORIZATION_LINK,
     }),
   ],
   debug: false,
   secret: AUTH_SECRET,
+  trustHost: true,
 };
 
 export const { GET, POST } = SolidAuth(authOpts);
